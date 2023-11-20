e61e5ce3fb5aaa3fda31976a57e371a795c435ff
dsildaikin <dsildaikin@gmail.com>
2023-11-20 21:28:17 +0300

    add ui

--- app/src/main/java/com/sildtech/calc/MainActivity.java
+++ app/src/main/java/com/sildtech/calc/MainActivity.java
@@ -4,42 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvOut;
-    Button btnOk;
-    Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // найдем View-элементы
-        tvOut = (TextView) findViewById(R.id.tvOut);
-        btnOk = (Button) findViewById(R.id.btnOk);
-        btnCancel = (Button) findViewById(R.id.btnCancel);
-
-        View.OnClickListener oclBtnOk = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Меняем текст в TextView (tvOut)
-                tvOut.setText("Нажата кнопка ОК");
-            }
-        };
-        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Меняем текст в TextView (tvOut)
-                tvOut.setText("Нажата кнопка Cancel");
-            }
-        };
-        // свяжем обработчик и кнопку OK (btnOk)
-        btnOk.setOnClickListener(oclBtnOk);
-        btnCancel.setOnClickListener(oclBtnCancel);
-
     }
 }
