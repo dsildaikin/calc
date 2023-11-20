90df56ea9045b7282e07b102104c9cff977292f2
dsildaikin <dsildaikin@gmail.com>
2023-11-20 18:21:54 +0300

    test buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.sildtech.calc"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sildtech.calc"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
